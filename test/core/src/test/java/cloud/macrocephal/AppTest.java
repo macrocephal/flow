32e9184c43e8d93be273fccf52c5758d10e19c42
Salathiel <salathiel@genese.name>
2023-09-20 10:13:04 +0200

    feat: first test cases with JPMS

---
+++ test/core/src/test/java/cloud/macrocephal/AppTest.java
@@ -0,0 +1,12 @@
+package cloud.macrocephal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
---
+++ test/core/src/test/java/module-info.java
@@ -0,0 +1,3 @@
+module test.flow.core {
+    requires test.flow.base;
+}
