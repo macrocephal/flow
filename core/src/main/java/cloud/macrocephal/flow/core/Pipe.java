a8f70cad257fdc2ca02847c44280d564b306716a
Salathiel <salathiel@genese.name>
2023-09-20 10:50:58 +0200

    feat: model pipes

--- core/src/main/java/cloud/macrocephal/flow/core/Pipe.java
+++ core/src/main/java/cloud/macrocephal/flow/core/Pipe.java
@@ -1,4 +1,21 @@
 package cloud.macrocephal.flow.core;
 
-public class Pipe {
+import cloud.macrocephal.flow.core.operator.Operator;
+
+import java.util.concurrent.Flow;
+
+public final class Pipe<T, U> {
+    private final Operator<T, U> operator;
+
+    public Pipe(Operator<T, U> operator) {
+        this.operator = operator;
+    }
+
+    public <V> Pipe<T, V> pipe(Operator<U, V> operator) {
+        return new Pipe<>(operand -> operator.apply(this.operator.apply(operand)));
+    }
+
+    public void subscribe(Flow.Subscriber<? super T> subscriber) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
