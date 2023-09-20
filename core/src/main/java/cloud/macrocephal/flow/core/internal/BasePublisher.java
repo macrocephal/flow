0929a8f6f319ccba5b22a864ace482772d4f3d62
Salathiel <salathiel@genese.name>
2023-09-20 15:19:24 +0200

    feat: init subscriber request count

--- core/src/main/java/cloud/macrocephal/flow/core/internal/BasePublisher.java
+++ core/src/main/java/cloud/macrocephal/flow/core/internal/BasePublisher.java
@@ -2,12 +2,17 @@ package cloud.macrocephal.flow.core.internal;
 
 import cloud.macrocephal.flow.core.Signal;
 
+import java.math.BigInteger;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.Flow;
 import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
 
 public class BasePublisher<T> implements Flow.Publisher<T> {
+    private final Map<Flow.Subscriber<? super T>, BigInteger> subscriberCount = new LinkedHashMap<>();
+
     protected BasePublisher(Consumer<Consumer<Signal<T>>> publishExposure) {
         requireNonNull(publishExposure);
         publishExposure.accept(this::publish);
@@ -16,6 +21,9 @@ public class BasePublisher<T> implements Flow.Publisher<T> {
     @Override
     public void subscribe(Flow.Subscriber<? super T> subscriber) {
         requireNonNull(subscriber);
+        subscriberCount.computeIfAbsent(subscriber, ignored -> {
+            return BigInteger.ZERO;
+        });
     }
 
     synchronized private void publish(Signal<T> signal) {
