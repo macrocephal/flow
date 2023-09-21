7e4c7c1d6d2f402742b369964893fa51e050482c
Salathiel <salathiel@genese.name>
2023-09-21 04:06:59 +0200

    feat: export exception package

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module cloud.macrocephal.flow.core {
     exports cloud.macrocephal.flow.core;
     exports cloud.macrocephal.flow.core.operator;
+    exports cloud.macrocephal.flow.core.exception;
 }
