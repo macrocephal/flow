599964fba10c8a1856ddc1c97c3cf894b827c101
Salathiel <salathiel@genese.name>
2023-09-30 14:33:44 +0200

    fix: remove maven shade plugin and drop JPMS in tests

--- test/core/src/test/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module test.flow.core {
-    requires cloud.macrocephal.flow.core;
-    requires org.reactivestreams.tck;
-    requires org.reactivestreams.tckflow;
-
-    requires test.flow.base;
-}
--- test/core/src/test/java/test/cloud/macrocephal/flow/core/publisher/internal/strategy/unicast/UnicastPullPublisherStrategyTest.java
+++ test/core/src/test/java/test/cloud/macrocephal/flow/core/publisher/internal/strategy/unicast/UnicastPullPublisherStrategyTest.java
@@ -1,23 +1,31 @@
 package test.cloud.macrocephal.flow.core.publisher.internal.strategy.unicast;
 
+import cloud.macrocephal.flow.core.Signal;
+import cloud.macrocephal.flow.core.publisher.Swarm;
+import cloud.macrocephal.flow.core.publisher.strategy.PublisherStrategy.Pull;
 import org.reactivestreams.tck.TestEnvironment;
 import org.reactivestreams.tck.flow.FlowPublisherVerification;
 
 import java.util.UUID;
 import java.util.concurrent.Flow.Publisher;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
-public class UnicastPullPublisherStrategyTest extends FlowPublisherVerification<UUID> {
-    public UnicastPullPublisherStrategyTest(TestEnvironment env, long publisherReferenceGCTimeoutMillis) {
-        super(env, publisherReferenceGCTimeoutMillis);
+import static java.util.UUID.randomUUID;
+
+public class UnicastPullPublisherStrategyTest extends FlowPublisherVerification<java.util.UUID> {
+    public UnicastPullPublisherStrategyTest() {
+        super(new TestEnvironment());
     }
 
     @Override
     public Publisher<UUID> createFlowPublisher(long l) {
-        return null;
+        return new Swarm<>(new Pull<>(() -> n -> LongStream.range(0, n)
+                .boxed().map(__ -> randomUUID()).map(Signal.Value::new)));
     }
 
     @Override
     public Publisher<UUID> createFailedFlowPublisher() {
-        return null;
+        return new Swarm<>(new Pull<>(() -> n -> Stream.of(new Signal.Error<>(new RuntimeException("Boom!")))));
     }
 }
