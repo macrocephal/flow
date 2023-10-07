8b2dee4ab6a4ffd3f26b4a810619552b80a2b771
Salathiel <salathiel@genese.name>
2023-10-07 12:27:53 +0200

    feat: constrains Single to emit at most one value

--- core/src/main/java/cloud/macrocephal/flow/core/publisher/Single.java
+++ core/src/main/java/cloud/macrocephal/flow/core/publisher/Single.java
@@ -1,10 +1,14 @@
 package cloud.macrocephal.flow.core.publisher;
 
 import cloud.macrocephal.flow.core.operator.Operator;
+import cloud.macrocephal.flow.core.operator.internal.NthOperator;
 import cloud.macrocephal.flow.core.publisher.internal.PublisherDefault;
 import cloud.macrocephal.flow.core.publisher.strategy.PublisherStrategy;
 
 import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+
+import static java.math.BigInteger.ONE;
 
 public final class Single<T> extends PublisherDefault<T> {
     public Single(Publisher<T> publisher) {
@@ -16,6 +20,11 @@ public final class Single<T> extends PublisherDefault<T> {
     }
 
     public <P extends Publisher<?>> P pipe(Operator<T, P> operator) {
-        return super.pipe(operator);
+        return super.pipe(operand -> operator.apply(new NthOperator<T>(ONE).apply(operand)));
+    }
+
+    @Override
+    public void subscribe(Subscriber<? super T> subscriber) {
+        pipe(operand -> operand).subscribe(subscriber);
     }
 }
