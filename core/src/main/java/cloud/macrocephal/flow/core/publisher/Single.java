c7bd47d07a3669e841cd57bf746730f802467016
Salathiel <salathiel@genese.name>
2023-10-07 21:51:22 +0200

    feat: add factory for empty & from source

--- core/src/main/java/cloud/macrocephal/flow/core/publisher/Single.java
+++ core/src/main/java/cloud/macrocephal/flow/core/publisher/Single.java
@@ -1,14 +1,20 @@
 package cloud.macrocephal.flow.core.publisher;
 
+import cloud.macrocephal.flow.core.Signal;
 import cloud.macrocephal.flow.core.operator.Operator;
 import cloud.macrocephal.flow.core.operator.internal.NthOperator;
 import cloud.macrocephal.flow.core.publisher.internal.PublisherDefault;
 import cloud.macrocephal.flow.core.publisher.strategy.PublisherStrategy;
+import cloud.macrocephal.flow.core.publisher.strategy.PublisherStrategy.Pull;
 
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
+import java.util.stream.Stream;
 
+import static cloud.macrocephal.flow.core.publisher.strategy.LagStrategy.ERROR;
 import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+import static java.util.Objects.requireNonNull;
 
 public final class Single<T> extends PublisherDefault<T> {
     public Single(Publisher<T> publisher) {
@@ -26,5 +32,17 @@ public final class Single<T> extends PublisherDefault<T> {
     @Override
     public void subscribe(Subscriber<? super T> subscriber) {
         pipe(operand -> operand).subscribe(subscriber);
+    }
+
+    public static <T> Single<T> empty() {
+        final var completeSignal = new Signal.Complete<T>();
+        return new Single<>(new Pull<>(ZERO, ERROR, () -> ignored -> Stream.of(completeSignal)));
+    }
+
+    public static <T> Single<T> of(T value) {
+        final var next = requireNonNull(value);
+        final var valueSignal = new Signal.Value<>(next);
+        final var completeSignal = new Signal.Complete<T>();
+        return new Single<>(new Pull<>(ZERO, ERROR, () -> ignored -> Stream.of(valueSignal, completeSignal)));
     }
 }
--- core/src/main/java/cloud/macrocephal/flow/core/publisher/Swarm.java
+++ core/src/main/java/cloud/macrocephal/flow/core/publisher/Swarm.java
@@ -1,10 +1,19 @@
 package cloud.macrocephal.flow.core.publisher;
 
+import cloud.macrocephal.flow.core.Signal;
 import cloud.macrocephal.flow.core.operator.Operator;
 import cloud.macrocephal.flow.core.publisher.internal.PublisherDefault;
 import cloud.macrocephal.flow.core.publisher.strategy.PublisherStrategy;
+import cloud.macrocephal.flow.core.publisher.strategy.PublisherStrategy.Pull;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.concurrent.Flow.Publisher;
+import java.util.stream.Stream;
+
+import static cloud.macrocephal.flow.core.publisher.strategy.LagStrategy.ERROR;
+import static java.math.BigInteger.ONE;
+import static java.util.Objects.requireNonNull;
 
 public final class Swarm<T> extends PublisherDefault<T> {
     public Swarm(Publisher<T> publisher) {
@@ -18,5 +27,18 @@ public final class Swarm<T> extends PublisherDefault<T> {
     @Override
     public <P extends Publisher<?>> P pipe(Operator<T, P> operator) {
         return super.pipe(operator);
+    }
+
+
+    public static <T> Swarm<T> empty() {
+        final var completeSignal = new Signal.Complete<T>();
+        return new Swarm<>(new Pull<>(ONE, ERROR, () -> ignored -> Stream.of(completeSignal)));
+    }
+
+    public static <T> Swarm<T> of(Collection<T> values) {
+        final var completeSignal = new Signal.Complete<T>();
+        final var next = requireNonNull(values);
+        return new Swarm<>(new Pull<>(ONE, ERROR, () -> ignored -> Stream.concat(
+                next.stream().map(Objects::requireNonNull).map(Signal.Value::new), Stream.of(completeSignal))));
     }
 }
